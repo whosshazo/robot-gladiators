076bed68f619b33108e91529f664bddd5a191b3e
whosshazo <ryandeshazo1@gmail.com>
2021-12-12 15:28:45 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,8 +7,14 @@ var playerMoney = 10;
 // console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy-Android", "Robo Trumble"]; console.log(enemyNames); 
-var enemyHealth = 15;
+var enemyHealth = randomNumber(40, 60)
 var enemyAttack = 12;
+
+var randomNumber = function() {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
 
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -24,14 +30,17 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    //generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -50,7 +59,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
