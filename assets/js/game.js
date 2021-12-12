5b39653a6a448492402ce88672df4b24a456862e
whosshazo <ryandeshazo1@gmail.com>
2021-12-12 13:02:34 -0500

    GAME OVER after player loses

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,9 +67,25 @@ var fight = function(enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+  if (playerHealth> 0) {
+    window.alert("Welcome to Robot Gladiators! Round" + ( i + 1 ));
+
+    /// pick a new enemy to fight based on the indez of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    //use debugger to pause script form running and check whats going on at that moment in code.
+    debugger;
+
+    // pass the pickedEnemyName variable's value into the fught function, where it will assume the value of the enemyname parameter.
+    fight(pickedEnemyName);
+
+  } else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  } 
 } 
   // run fight function to start game
   fight();
